21260a5719edf3856e857890f48b2d3318d974cb
omonmunduz <omurbekn@gmail.com>
2021-04-26 11:06:17 -0700

    gatsby-config configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SkyGo
+    Global SIM`,
+    description: `SkyGo
+    Global SIM and Global Packages
+    One world. One operator. One affordable price. Get internet connection in 100+ countries worldwide, with a validity of 180 days.`,
+    author: `Omur Nazar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
